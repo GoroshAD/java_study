cbc8068e3efb0fd0ffad7313134fed57f00547e0
GoroshAD <144345407+GoroshAD@users.noreply.github.com>
2025-03-20 22:13:28 +0300

    Delete SecondTask.java

--- SecondTask.java
+++
@@ -1,7 +0,0 @@
-public class SecondTask {
-    public static void main(String[] args) {
-        int income = 44000;
-        int spending = 30000;
-        System.out.println(income - spending);
-    }
-}
