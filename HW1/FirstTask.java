f7cabe171dacda632d872273653a07190df5a047
GoroshAD <144345407+GoroshAD@users.noreply.github.com>
2025-03-20 22:18:15 +0300

    Added homework

---
+++ HW1/FirstTask.java
@@ -0,0 +1,7 @@
+public class FirstTask {
+    public static void main(String[] args) {
+        String name = "Artiom";   //or Artem, but usually i write it like this
+        String surname = "Goroshko";
+        System.out.println(name+" "+surname);
+    }
+}
---
+++ HW1/SecondTask.java
@@ -0,0 +1,7 @@
+public class SecondTask {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+        System.out.println(income - spending);
+    }
+}
