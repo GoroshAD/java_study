2f934e4fcd6ac3d8f5d7c2b7ebd966de5a978453
GoroshAD <144345407+GoroshAD@users.noreply.github.com>
2025-03-20 22:13:43 +0300

    Delete FirstTask.java

--- FirstTask.java
+++
@@ -1,7 +0,0 @@
-public class FirstTask {
-    public static void main(String[] args) {
-        String name = "Artiom";   //or Artem, but usually i write it like this
-        String surname = "Goroshko";
-        System.out.println(name+" "+surname);
-    }
-}
